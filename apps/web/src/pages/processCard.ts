1cea89a0a388f0e647b25739530a4e4a7e04464d
Apple <appleteng97@gmail.com>
2022-11-09 22:22:08 +0800

    refactor: amend final card b4 adding number of card created to the obj

--- apps/web/src/pages/processCard.ts
+++ apps/web/src/pages/processCard.ts
@@ -20,6 +20,8 @@ const fetchData = () =>{
       {month:'November'},
       {month:'December'}
     ]
+    
+    final_card_for_chart = restructureFinalCard(final_card_for_chart)
 
     for (let card_status in chartData){
       for (let month in chartData[card_status]){
@@ -45,6 +47,39 @@ const fetchData = () =>{
   .catch (error => {
     console.log(error);
   });
+}
+
+const restructureFinalCard = (final_card) =>{
+  for (let i = 0; i<final_card.length; i++){
+    let card = final_card[i]
+    card['作業'] = 0                 
+    card['Productivity'] = 0
+    card['VS Code'] = 0              
+    card['Video'] = 0
+    card['Linux'] = 0
+    card['Reading'] = 0
+    card['Doc'] = 0
+    card['Done'] = 0
+    card['Trello'] = 0
+    card['Project Management'] = 0
+    card['Class'] = 0
+    card['自學'] = 0
+    card['Interview'] = 0
+    card['Testing'] = 0
+    card['CI/CD'] = 0
+    card['IDE'] = 0
+    card['Github'] = 0
+    card['HackMD'] = 0
+    card['Software Engineering'] = 0
+    card['Git'] = 0
+    card['Semantic Versioning'] = 0
+    card['Markdown'] = 0
+    card['SSH'] = 0
+    card['Meeting'] = 0
+    card['Feature'] = 0
+  }
+
+  return final_card
 }
 
 export default fetchData

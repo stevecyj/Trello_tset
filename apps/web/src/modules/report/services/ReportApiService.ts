9781b40ffca932999738f7c3344f02868b49a933
Kos Huang <koshuang@gmail.com>
2022-11-19 16:15:06 +0800

    refactor(report/page): [MyReoprt] extract transformer for api data

--- apps/web/src/modules/report/services/ReportApiService.ts
+++ apps/web/src/modules/report/services/ReportApiService.ts
@@ -1,12 +1,15 @@
+import { monthlyCreatedTasksGroupByLabelTransformer } from './../transformers/MonthlyCreatedTasksGroupByLabelTransformer';
 import axios from 'axios'
 class ReportApiService {
   async getChart(query: any) {
     const params = new URLSearchParams(query)
     const url = 'http://localhost:3000/api/reports/chart?'+params
     const getCards = await axios.get(url)
-    const card = getCards.data
+    const card = getCards.data;
 
-    return card;
+    const reportData = monthlyCreatedTasksGroupByLabelTransformer.transform(card);
+
+    return reportData;
   }
 }
 

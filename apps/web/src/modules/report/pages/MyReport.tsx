337735be1d8c92e45abb0b433d9e796c4180aac4
Kos Huang <koshuang@gmail.com>
2022-11-19 17:22:17 +0800

    refactor(report/page): [MyReport] extract MonthlyCreatedTasksGroupByLabelSettings component

--- apps/web/src/modules/report/pages/MyReport.tsx
+++ apps/web/src/modules/report/pages/MyReport.tsx
@@ -1,9 +1,7 @@
-import { useState, useEffect } from 'react';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
 import { useRouter } from "next/router"
 import { reportApiService } from '../services/ReportApiService';
 import { MonthlyCreatedTasksGroupByLabel } from '../../chart-templates/components/MonthlyCreatedTasksGroupByLabel';
+import { MonthlyCreatedTasksGroupByLabelSettings } from '../../chart-templates/components/MonthlyCreatedTasksGroupByLabelSettings';
 
 export const getServerSideProps = async(context) =>{
   const {
@@ -20,23 +18,12 @@ export const getServerSideProps = async(context) =>{
 }
 
 export const MyReport = ({ result, labels }) =>{
-  const [status, setStatus] = useState('')
-  const [label, setLabel] = useState('')
-  const [from, setFrom] = useState('')
-  const [to, setTo] = useState('')
-
   const router = useRouter()
 
-  const handleSubmit = (e) =>{
-    e.preventDefault();
+  const handleSubmit = (query) =>{
     router.replace({
       pathname:router.asPath,
-      query:{
-        status:status,
-        /* label:label,
-        from:from,
-        to:to */
-      }
+      query
     })
     /*let url_with_filter = defaulturl + "?"
     if(status){
@@ -55,56 +42,16 @@ export const MyReport = ({ result, labels }) =>{
     fetchData(url_with_filter) */
   }
 
-  const fetchAll = (e) =>{
-    /* e.preventDefault();
+  const fetchAll = () =>{
+    /*
     defaulturl = 'http://localhost:3000/api/reports/chart';
     fetchData(defaulturl) */
-    e.preventDefault()
     console.log(router.asPath)
   }
 
   return(
     <div>
-      <div id="UserInput">
-        <form onSubmit={handleSubmit}>
-          <TextField
-          label="Status"
-          variant="filled"
-          sx={{ mb: 2 }}
-          value={status}
-          onChange={e => setStatus(e.target.value)}
-          /><br />
-
-          <TextField
-            label="Label"
-            variant="filled"
-            sx={{ mb: 2 }}
-            value={label}
-            onChange={e => setLabel(e.target.value)}
-          /><br />
-
-          <TextField
-            label="From"
-            variant="filled"
-            type='date'
-            sx={{ mb: 2 }}
-            value={from}
-            onChange={e => setFrom(e.target.value)}
-          /><br />
-
-          <TextField
-            label="To"
-            variant="filled"
-            type='date'
-            sx={{ mb: 2 }}
-            value={to}
-            onChange={e => setTo(e.target.value)}
-          /><br />
-
-          <Button variant='outlined' color='success' type='submit'>Filter Data</Button>
-          <Button variant='contained' color='success' onClick={fetchAll} sx={{ ml: 2 }}>Fetch All</Button>
-        </form>
-      </div>
+      <MonthlyCreatedTasksGroupByLabelSettings onSubmit={handleSubmit} fetchAll={fetchAll} />
       <MonthlyCreatedTasksGroupByLabel result={result} labels={labels} />
     </div>
   )

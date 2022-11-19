26ed6fedc55bbf710ad48204e2bfa19f3dc02002
Kos Huang <koshuang@gmail.com>
2022-11-19 16:48:29 +0800

    chore(report/trasformer): can get labels dynamically

--- apps/web/src/modules/report/transformers/MonthlyCreatedTasksGroupByLabelTransformer.ts
+++ apps/web/src/modules/report/transformers/MonthlyCreatedTasksGroupByLabelTransformer.ts
@@ -1,41 +1,33 @@
+import _ from 'lodash';
+
 class MonthlyCreatedTasksGroupByLabelTransformer {
   public transform(raw) {
     return this.processCard(raw);
   }
 
-  private processCard(card) {
-    const restructureFinalCard = (final_card) =>{
-      for (let i = 0; i<final_card.length; i++){
-        let card = final_card[i]
-        card['作業'] = 0
-        card['Productivity'] = 0
-        card['VS Code'] = 0
-        card['Video'] = 0
-        card['Linux'] = 0
-        card['Reading'] = 0
-        card['Doc'] = 0
-        card['Done'] = 0
-        card['Trello'] = 0
-        card['Project Management'] = 0
-        card['Class'] = 0
-        card['自學'] = 0
-        card['Interview'] = 0
-        card['Testing'] = 0
-        card['CI/CD'] = 0
-        card['IDE'] = 0
-        card['Github'] = 0
-        card['HackMD'] = 0
-        card['Software Engineering'] = 0
-        card['Git'] = 0
-        card['Semantic Versioning'] = 0
-        card['Markdown'] = 0
-        card['SSH'] = 0
-        card['Meeting'] = 0
-        card['Feature'] = 0
-        card['No Label'] = 0
-      }
+  private getLabels(raw): string[] {
+    const level1 = _.transform(raw, function(result, value, key) {
+      _.merge(result, value);
+    }, {});
 
-      return final_card
+    const level2 = _.transform(level1, function(result, value, key) {
+      _.merge(result, value);
+    }, {});
+
+    const labels = Object.keys(level2);
+
+    return labels;
+  }
+
+  private processCard(card) {
+    const labels = this.getLabels(card);
+
+    const restructureFinalCard = (final_card) =>{
+      return final_card.map((item) => {
+        labels.forEach(label => item[label] = 0);
+
+        return item;
+      });
     }
 
     let final_card_for_chart = [
@@ -77,5 +69,87 @@ class MonthlyCreatedTasksGroupByLabelTransformer {
     return final_card_for_chart;
   }
 }
+
+/*
+Data example
+
+{
+  "Backlog": {
+    "October": { "No Label": 1, "Feature": 1 },
+    "July": { "No Label": 2 },
+    "April": { "No Label": 1 },
+    "June": { "No Label": 6 },
+    "May": { "No Label": 6 },
+    "August": { "No Label": 2 },
+    "February": { "No Label": 1 }
+  },
+  "Todo": { "November": { "Feature": 2 } },
+  "In Progress": {
+    "October": { "Feature": 1, "Frontend": 1, "Backend": 1 },
+    "November": { "作業": 2 },
+    "June": { "No Label": 2 }
+  },
+  "Done": {
+    "September": { "No Label": 2 },
+    "June": {
+      "No Label": 5,
+      "作業": 4,
+      "Productivity": 1,
+      "VS Code": 1,
+      "Video": 1
+    },
+    "April": { "No Label": 2, "作業": 3 },
+    "August": { "No Label": 4 },
+    "May": {
+      "No Label": 7,
+      "Linux": 2,
+      "作業": 3,
+      "Reading": 1,
+      "Doc": 1,
+      "Done": 1,
+      "Trello": 1,
+      "Project Management": 1,
+      "Productivity": 2
+    },
+    "July": { "No Label": 3 },
+    "December": { "No Label": 1 }
+  },
+  "Classes": {
+    "June": {
+      "Productivity": 1,
+      "VS Code": 1,
+      "Class": 3,
+      "自學": 1,
+      "Video": 3
+    },
+    "August": { "Trello": 1, "Video": 4, "Class": 5 },
+    "July": { "Class": 1, "Video": 2 },
+    "May": {
+      "Interview": 1,
+      "Class": 4,
+      "Testing": 1,
+      "CI/CD": 1,
+      "IDE": 1,
+      "Github": 2,
+      "Video": 4,
+      "HackMD": 1,
+      "Software Engineering": 1,
+      "Git": 2,
+      "Semantic Versioning": 1,
+      "Markdown": 1,
+      "SSH": 1,
+      "Linux": 1
+    }
+  },
+  "Closed": {
+    "November": { "No Label": 1 },
+    "October": { "No Label": 1, "作業": 1 },
+    "April": { "Meeting": 1 }
+  },
+  "General Info": { "June": { "No Label": 2 }, "April": { "Doc": 1 } },
+  "Templates": { "April": { "Feature": 1 } }
+}
+
+*/
 
 export const monthlyCreatedTasksGroupByLabelTransformer = new MonthlyCreatedTasksGroupByLabelTransformer();

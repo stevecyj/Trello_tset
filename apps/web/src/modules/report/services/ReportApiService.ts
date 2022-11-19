e757b6f47695ca570c5328ed4a1096e37d393a20
Kos Huang <koshuang@gmail.com>
2022-11-19 15:57:45 +0800

    refactor(report/page): [MyReport] extract ReportApiService

---
+++ apps/web/src/modules/report/services/ReportApiService.ts
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+class ReportApiService {
+  async getChart(query: any) {
+    const params = new URLSearchParams(query)
+    const url = 'http://localhost:3000/api/reports/chart?'+params
+    const getCards = await axios.get(url)
+    const card = getCards.data
+
+    return card;
+  }
+}
+
+
+export const reportApiService = new ReportApiService();

16ad200ced8a2cb1f52132469f0078c97c7aa092
Apple <appleteng97@gmail.com>
2022-11-26 13:29:30 +0800

    amend

---
+++ apps/web/src/modules/report/pages/ChartCardDetails.tsx
@@ -0,0 +1,29 @@
+import { reportApiService } from '../services/ReportApiService';
+import { MonthlyCreatedTasksGroupByLabel } from '../../chart-templates/components/MonthlyCreatedTasksGroupByLabel';
+import { NavigationMenu } from '../../chart-templates/components/NavigationMenu';
+//import { MonthlyCreatedTasksGroupByLabelSettings } from '../../chart-templates/components/MonthlyCreatedTasksGroupByLabelSettings';
+
+export const getServerSideProps = async(context) =>{
+    const {
+        reportData: result,
+        labels
+      } = await reportApiService.getChart(context.query);
+
+    return{
+        props:{
+            result,
+            labels
+        }
+    }
+}
+
+export const ChartCardDetails = ({result, labels}) =>{
+
+  return(
+    <div>
+      {/* <MonthlyCreatedTasksGroupByLabelSettings onSubmit={handleSubmit} /> */}
+      <NavigationMenu></NavigationMenu>
+      <MonthlyCreatedTasksGroupByLabel result={result} labels={labels}></MonthlyCreatedTasksGroupByLabel>
+    </div>
+  )
+}
---
+++ apps/web/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { ChartCards } from "../modules/chart-templates/components/ChartCards"
+import { NavigationMenu } from "../modules/chart-templates/components/NavigationMenu"
+
+export const IndexPage = () =>{
+    return(
+        <div>
+            <NavigationMenu></NavigationMenu>
+            <ChartCards></ChartCards>
+        </div>
+        
+    )
+}
+
+export default IndexPage
